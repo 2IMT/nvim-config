4b9407fc8a85511194a51650421855239f7cfc54
2IMT <daniil_t04@mail.ru>
2024-03-27 02:26:07 +0300

    Add bufferline.nvim keybinds

--- lua/daniil/plugins/bufferline.lua
+++ lua/daniil/plugins/bufferline.lua
@@ -7,7 +7,16 @@ return {
         bufferline.setup {
             options = {
                 buffer_close_icon = '',
+                diagnostics = "coc"
             }
         }
+
+        vim.keymap.set("n", "<leader>5", "<cmd>BufferLineCycleNext<CR>")
+        vim.keymap.set("n", "<leader>4", "<cmd>BufferLineCyclePrev<CR>")
+        vim.keymap.set("n", "<leader>bcl", "<cmd>BufferLineCloseLeft<CR>")
+        vim.keymap.set("n", "<leader>bcr", "<cmd>BufferLineCloseRight<CR>")
+        vim.keymap.set("n", "<leader>bco", "<cmd>BufferLineCloseOthers<CR>")
+        vim.keymap.set("n", "<leader>bg", "<cmd>BufferLinePick<CR>")
+        vim.keymap.set("n", "<leader>bpc", "<cmd>BufferLinePickClose<CR>")
     end
 }
