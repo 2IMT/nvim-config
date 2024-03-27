ad6c96757c5ad7184d9e82201b746fa172b67c29
2IMT <daniil_t04@mail.ru>
2024-03-27 16:54:12 +0300

    Add coc.nvim keybinds

--- lua/2imt/plugins/coc.lua
+++ lua/2imt/plugins/coc.lua
@@ -1,7 +1,26 @@
 return {
-	"neoclide/coc.nvim",
-	branch = "release",
-	config = function()
-		vim.keymap.set("n", "<leader>f", "<cmd>call CocAction('format')<CR>")
-	end
+    "neoclide/coc.nvim",
+    branch = "release",
+    config = function()
+        vim.keymap.set("n", "<leader>f", "<cmd>call CocAction('format')<CR>")
+
+        vim.keymap.set("n", "<leader>cdd", "<cmd>CocDiagnostics<CR>")
+        vim.keymap.set("n", "<leader>cdn", "<Plug>(coc-diagnostic-next)")
+        vim.keymap.set("n", "<leader>cdp", "<Plug>(coc-diagnostic-prev)")
+
+        vim.keymap.set("n", "<leader>cdf", "<Plug>(coc-definition)")
+        vim.keymap.set("n", "<leader>ctd", "<Plug>(coc-type-definition)")
+        vim.keymap.set("n", "<leader>cim", "<Plug>(coc-implementation)")
+        vim.keymap.set("n", "<leader>cdr", "<Plug>(coc-references)")
+
+        vim.keymap.set("n", "<leader>cac", "<Plug>(coc-codeaction-cursor)")
+        vim.keymap.set("x", "<leader>cas", "<Plug>(coc-codeaction-selected)")
+        vim.keymap.set("n", "<leader>cas", "<Plug>(coc-codeaction-source)")
+        vim.keymap.set("n", "<leader>car", "<Plug>(coc-codeaction-refactor)")
+        vim.keymap.set("x", "<leader>car", "<Plug>(coc-codeaction-refactor-selected)")
+
+        vim.keymap.set("n", "<leader>cr", "<Plug>(coc-rename)")
+
+        vim.keymap.set("n", "<leader>cl", "<Plug>(coc-codelens-action)")
+    end
 }
