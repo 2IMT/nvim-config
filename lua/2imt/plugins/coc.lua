cedfae3aedae757945069a5ab02eca1a4e487c8c
2IMT <daniil_t04@mail.ru>
2024-03-27 17:01:14 +0300

    Add options to coc.nvim keybinds

--- lua/2imt/plugins/coc.lua
+++ lua/2imt/plugins/coc.lua
@@ -2,25 +2,28 @@ return {
     "neoclide/coc.nvim",
     branch = "release",
     config = function()
-        vim.keymap.set("n", "<leader>f", "<cmd>call CocAction('format')<CR>")
+        local opts = { silent = true }
+
+        vim.keymap.set("n", "<leader>f", "<cmd>call CocAction('format')<CR>", opts)
 
         vim.keymap.set("n", "<leader>cdd", "<cmd>CocDiagnostics<CR>")
-        vim.keymap.set("n", "<leader>cdn", "<Plug>(coc-diagnostic-next)")
-        vim.keymap.set("n", "<leader>cdp", "<Plug>(coc-diagnostic-prev)")
+        vim.keymap.set("n", "<leader>cdn", "<Plug>(coc-diagnostic-next)", opts)
+        vim.keymap.set("n", "<leader>cdp", "<Plug>(coc-diagnostic-prev)", opts)
 
-        vim.keymap.set("n", "<leader>cdf", "<Plug>(coc-definition)")
-        vim.keymap.set("n", "<leader>ctd", "<Plug>(coc-type-definition)")
-        vim.keymap.set("n", "<leader>cim", "<Plug>(coc-implementation)")
-        vim.keymap.set("n", "<leader>cdr", "<Plug>(coc-references)")
+        vim.keymap.set("n", "<leader>cdf", "<Plug>(coc-definition)", opts)
+        vim.keymap.set("n", "<leader>ctd", "<Plug>(coc-type-definition)", opts)
+        vim.keymap.set("n", "<leader>cim", "<Plug>(coc-implementation)", opts)
+        vim.keymap.set("n", "<leader>cdr", "<Plug>(coc-references)", opts)
 
-        vim.keymap.set("n", "<leader>cac", "<Plug>(coc-codeaction-cursor)")
-        vim.keymap.set("x", "<leader>cas", "<Plug>(coc-codeaction-selected)")
-        vim.keymap.set("n", "<leader>cas", "<Plug>(coc-codeaction-source)")
-        vim.keymap.set("n", "<leader>car", "<Plug>(coc-codeaction-refactor)")
-        vim.keymap.set("x", "<leader>car", "<Plug>(coc-codeaction-refactor-selected)")
+        vim.keymap.set("n", "<leader>cac", "<Plug>(coc-codeaction-cursor)", opts)
+        vim.keymap.set("x", "<leader>cas", "<Plug>(coc-codeaction-selected)", opts)
+        vim.keymap.set("n", "<leader>cas", "<Plug>(coc-codeaction-source)", opts)
+        vim.keymap.set("n", "<leader>car", "<Plug>(coc-codeaction-refactor)", opts)
+        vim.keymap.set("x", "<leader>car", "<Plug>(coc-codeaction-refactor-selected)", opts)
 
-        vim.keymap.set("n", "<leader>cr", "<Plug>(coc-rename)")
+        vim.keymap.set("n", "<leader>cr", "<Plug>(coc-rename)", opts)
 
-        vim.keymap.set("n", "<leader>cl", "<Plug>(coc-codelens-action)")
+        vim.keymap.set("n", "<leader>cl", "<Plug>(coc-codelens-action)",
+            { silent = true, nowait = true })
     end
 }
