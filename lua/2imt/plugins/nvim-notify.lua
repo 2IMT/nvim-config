b5b9af872fbc11cf47689fcb25e6ba6a62a6468b
2IMT <daniil_t04@mail.ru>
2024-03-31 12:00:57 +0300

    Add keybind to dismiss notifications

--- lua/2imt/plugins/nvim-notify.lua
+++ lua/2imt/plugins/nvim-notify.lua
@@ -3,5 +3,9 @@ return {
     priority = 999,
     config = function()
         vim.notify = require("notify")
+
+        vim.keymap.set("n", "<leader>nd", function()
+            vim.notify.dismiss()
+        end)
     end
 }
