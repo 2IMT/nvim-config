c3ac246b1afaf92adb86df16f4fa4058a9938388
2IMT <daniil_t04@mail.ru>
2024-04-06 10:24:30 +0300

    Add glsl to tresitter

--- lua/2imt/plugins/nvim-treesitter.lua
+++ lua/2imt/plugins/nvim-treesitter.lua
@@ -2,7 +2,7 @@ return {
     "nvim-treesitter/nvim-treesitter",
     config = function()
         require("nvim-treesitter.configs").setup({
-            ensure_installed = { "c", "cpp", "rust", "lua", "python" },
+            ensure_installed = { "c", "cpp", "rust", "lua", "python", "glsl" },
             highlight = {
                 enable = true
             }
